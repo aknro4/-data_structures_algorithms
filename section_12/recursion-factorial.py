13ccc081efa30adb29ad418e9a9f6a474006fbca
aknro4 <aarokolu4@gmail.com>
2023-07-03 10:10:41 +0300

    Recursive basics

---
+++ section_12/recursion-factorial.py
@@ -0,0 +1,24 @@
+answer_1 = 1
+answer_2 = 1
+
+
+def find_factorial_recursive(number):
+    global answer_1
+    answer_1 *= number
+    number -= 1
+    if number == 0:
+        return answer_1
+    return find_factorial_recursive(number)
+
+
+# Loops
+def find_factorial_iterative(number):
+    global answer_2
+    while number != 0:
+        answer_2 *= number
+        number -= 1
+    return answer_2
+
+
+print(find_factorial_recursive(5))
+print(find_factorial_iterative(5))
